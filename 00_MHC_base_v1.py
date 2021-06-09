0c864ac28fd1dcfdf0d4adfd47c12cb6d7a79b63
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-06-09 14:49:57 +1200

    Create 00_MHC_base_v1.py

---
+++ 00_MHC_base_v1.py
@@ -0,0 +1,28 @@
+# import statement
+
+
+# function goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ********** Main Routine **********
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask the user if they have used the program before & show instruction if necessary
+
+# Loop to get ticket details
+
+ # start of loop
