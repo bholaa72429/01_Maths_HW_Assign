6c464e30be6c362db934800071f581fa8c6eabc3
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-06-16 14:53:03 +1200

    dev

--- 03_circle_v1.py
+++ 03_circle_v1.py
@@ -1,25 +1,27 @@
 # areperi = area and perimeter
-def circle_areperi(side):
+def circle_areperi(radius):
 
    valid = False
 
    error = "Whoops! Please enter an integer "
+   int_error = "Ohh! Please enter an number more than zero "
    while not valid:
        try:
            # get users input (radius)
-           side = int(input(side))
+           radius = int(input(radius))
 
             # check if its more than 0 nad then calculate
-           if side > 0 :
-               area = 3.14*side*side
-               peri = 2 * 3.14 * side
+           if radius > 0 :
+               area = 3.14*radius*radius
+               peri = 2 * 3.14 * radius
 
                return area, peri
            # if not more than zero show error
            else:
-               print("Please enter an number more than zero")
+               print(int_error)
                print()
 
+       # If integer is not entered, show error
        except ValueError:
            print(error)
 
@@ -27,13 +29,13 @@ area = ""
 keep_going = ""
 while keep_going == "":
 
-   ap = circle_areperi("Please enter Radius of the Circle. ")
-   print("Area and Perimeter of Circle respectively",ap)
+   circle_ap = circle_areperi("Please enter Radius of the Circle. ")
+   print("Area and Perimeter of Circle respectively",circle_ap)
 
 
-    # ask for more or quit
+   # ask for more or quit
    keep_going = input("Press <enter> to calculate more or any key to quit")
 
 # farewell user at end of game.
-print("Thank you")
+print("----Thank you----")
 
