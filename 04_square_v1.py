395dceb38e70cba5457b41562225ec74567e0a53
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-06-25 12:20:22 +1200

    nt workin

--- 04_square_v1.py
+++ 04_square_v1.py
@@ -4,6 +4,7 @@
 def square_area_peri(side):
    valid = False
    error = "Whoops! Please enter an integer "
+   int_error = "ohh! Please enter an number more than zero"
    while not valid:
        try:
            side = int(input(side))
@@ -14,28 +15,35 @@ def square_area_peri(side):
 
                return area, peri
            else:
-               print(error)
-               print()
+               print(int_error)
+
 
        except ValueError:
            print(error)
 
 # main routine
-area = ""
+area_sqr = ""
 keep_going = ""
 while keep_going == "":
 
-   area_peri = square_area_peri("Please enter side of the Square")
-   area, peri = square_area_peri(area_peri)
-   print("Area of Square", area)
-   print("Perimeter of Square", peri)
+   area_sqr, peri_sqr = square_area_peri("Please enter side of the Square. ")
+   print("Calculating...")
+   print("Area of Square", area_sqr)
+   print("Perimeter of Square", peri_sqr)
 
 
 
-
+   print()
    keep_going = input("Press <enter> to calculate more or any key to quit")
 
 # farewell user at end of game.
 print("Thank you")
 
-
+# 0 wali problem sqr
+# sab function may same name ?
+# adding to base start kari !!
+# --
+# wed - pre sub
+# rounding & unit
+# ques - "int check to save time 0 " - will it work 2 fuction for 1 question
+# put base !!
