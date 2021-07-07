0d7ebd0b6c7c406e85851a96714818f5f40d9ec4
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-07-07 20:32:54 +1200

    Update 00_sandbox.py

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -3,17 +3,17 @@
 # get input
 
 # Trial # 1
-# given_data = ""
-# for item in range(0, 3):
-#     side = int(input("Length? "))
-#     given_data += "side length: " + str(side)
+given_data = ""
+for item in range(0, 3):
+    side = int(input("Length? "))
+    given_data += "side length: " + str(side)
+
+print("given data: ", given_data)
+
+# units = "cm"
+# side = input("side length: ")
+# base = input("base: ")
 #
-# print("given data: ", given_data)
-
-units = "cm"
-side = input("side length: ")
-base = input("base: ")
-
-given_data = "side length: {} {} | base: {} {}".format(side, units, base, units)
-
-print(given_data)
+# given_data = "side length: {} {} | base: {} {}".format(side, units, base, units)
+#
+# print(given_data)
